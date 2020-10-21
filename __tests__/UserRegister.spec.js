2901ebe6c9d9c9d1c76b356ea5ab7cd1f2816327
glyxerin3 <glyxkind@gmx.de>
2020-10-21 16:40:36 +0200

    Add Validation sequelize

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -3,18 +3,18 @@ const app = require('../src/app');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
 
-describe('User Registration', () => {
+const validUser = {
+  username: 'user1',
+  email: 'user1@gmail.com',
+  password: 'password'
+};
 
-  const postValidUser = async () => {
-    return request(app).post('/api/1.0/users')
-      .send(
-        {
-          username: 'user1',
-          email: 'user1@gmail.com',
-          password: 'password'
-        }
-      );
-  };
+const postUser = (user = validUser) => {
+  return request(app).post('/api/1.0/users')
+    .send(user);
+};
+
+describe('User Registration', () => {
 
   beforeAll(() => {
     return sequelize.sync();
@@ -26,23 +26,23 @@ describe('User Registration', () => {
 
 
   it('returns 200 OK when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.status).toBe(200);
   });
 
   it('returns success message when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.body.message).toBe('User created');
   });
 
   it('saves the user to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it('saves username and email to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.username).toBe('user1');
@@ -50,10 +50,45 @@ describe('User Registration', () => {
   });
 
   it('hashes the password in database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.password).not.toBe('password');
   });
 
+  it('returns 400 when username is null', async () => {
+    const response = await postUser(
+      {
+        username: null,
+        email: 'user1@gmail.com',
+        password: 'password'
+      }
+    );
+
+    expect(response.status).toBe(400);
+  });
+
+  it('returns validationErros field in response body when validation error occurs', async () => {
+    const response = await postUser(
+      {
+        username: null,
+        email: 'user1@gmail.com',
+        password: 'password'
+      }
+    );
+    const body = response.body;
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('returns Username cannot be null when username is null', async () => {
+    const response = await postUser(
+      {
+        username: null,
+        email: 'user1@gmail.com',
+        password: 'password'
+      }
+    );
+    const body = response.body;
+    expect(body.validationErrors.username).toBe('Username cannot be null');
+  });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,6 +4,25 @@ require('dotenv').config();
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
+  const user = req.body;
+
+  if (user.username === null) {
+    return res.status(400).send(
+      {
+        validationErrors: {
+          username: 'Username cannot be null'
+        }
+      }
+    );
+  }
+
+  // {
+  //   validationErrors: {
+  //     username: 'Username cannot be null',
+  //       email: 'Email cannot be null'
+  //   }
+  // }
+
   await UserService.save(req.body);
 
   return res.send(
