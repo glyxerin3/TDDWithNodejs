77e95639e94b9e720ad6e44c222c9f9a0530ebb9
glyxerin3 <glyxkind@gmx.de>
2020-10-22 21:56:41 +0200

    returns erros for both when username and email is null

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,26 +3,34 @@ const UserService = require('./UserService');
 require('dotenv').config();
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
+const validateUsername = (req, res, next) => {
   const user = req.body;
 
   if (user.username === null) {
-    return res.status(400).send(
-      {
-        validationErrors: {
-          username: 'Username cannot be null'
-        }
-      }
-    );
+    req.validationErrors = {
+      username: 'Username cannot be null'
+    };
   }
+  next();
+};
 
-  // {
-  //   validationErrors: {
-  //     username: 'Username cannot be null',
-  //       email: 'Email cannot be null'
-  //   }
-  // }
+const validateEmail = (req, res, next) => {
+  const user = req.body;
 
+  if (user.email === null) {
+    req.validationErrors = {
+      ...req.validationErrors,
+      email: 'Email cannot be null'
+    };
+  }
+  next();
+};
+
+router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
+  if (req.validationErrors) {
+    const response = { validationErrors: {...req.validationErrors}};
+    return res.status(400).send(response);
+  }
   await UserService.save(req.body);
 
   return res.send(
