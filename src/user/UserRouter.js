ab66c258e648e6f60e321346d7771738754ba1a3
glyxerin3 <glyxkind@gmx.de>
2020-10-26 18:41:14 +0100

    Replace validation middleware with express-validator

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserService = require('./UserService');
 require('dotenv').config();
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 const validateUsername = (req, res, next) => {
   const user = req.body;
@@ -26,11 +27,18 @@ const validateEmail = (req, res, next) => {
   next();
 };
 
-router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
-  if (req.validationErrors) {
-    const response = { validationErrors: {...req.validationErrors}};
-    return res.status(400).send(response);
+router.post('/api/1.0/users',
+  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('email').notEmpty().withMessage('Email cannot be null'),
+  async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const validationErrors = {};
+    errors.array().forEach(error => validationErrors[error.param] = error.msg);
+    return res.status(400).send({validationErrors});
   }
+
   await UserService.save(req.body);
 
   return res.send(
