8ebea9546bc369d051f750a802d5d02bf318940c
glyxerin3 <glyxkind@gmx.de>
2020-10-26 18:47:45 +0100

    returns Password cannot be null message when password is null

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,32 +4,10 @@ require('dotenv').config();
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 
-const validateUsername = (req, res, next) => {
-  const user = req.body;
-
-  if (user.username === null) {
-    req.validationErrors = {
-      username: 'Username cannot be null'
-    };
-  }
-  next();
-};
-
-const validateEmail = (req, res, next) => {
-  const user = req.body;
-
-  if (user.email === null) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: 'Email cannot be null'
-    };
-  }
-  next();
-};
-
 router.post('/api/1.0/users',
   check('username').notEmpty().withMessage('Username cannot be null'),
   check('email').notEmpty().withMessage('Email cannot be null'),
+  check('password').notEmpty().withMessage('Password cannot be null'),
   async (req, res) => {
 
   const errors = validationResult(req);
