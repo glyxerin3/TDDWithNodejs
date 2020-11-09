bba1402701d2b2ec02fdec80a9601622f17469e2
glyxerin3 <glyxkind@gmx.de>
2020-11-10 00:10:40 +0100

    Added Internationalisation MongoDB

--- __tests__/UserRegisterMongoDB.spec.js
+++ __tests__/UserRegisterMongoDB.spec.js
@@ -13,9 +13,14 @@ const validUser = {
   password: 'P4ssword'
 };
 
-const postUser = (user = validUser) => {
-  return request(app).post(REGISTER_ENDPOINT_MONGODB)
-    .send(user);
+const postUser = (user = validUser, options = {language: 'en'}) => {
+  const agent = request(app).post(REGISTER_ENDPOINT_MONGODB);
+
+  if (options.language) {
+    agent.set('Accept-Language', options.language)
+  }
+
+  return agent.send(user);
 };
 
 describe('User Registration MongoDB', () => {
@@ -41,7 +46,7 @@ describe('User Registration MongoDB', () => {
 
   it('returns success message when signup request is valid', async () => {
     const response = await postUser();
-    expect(response.body.message).toBe('User created');
+    expect(response.body.message).toBe(user_create_success);
   });
 
   it('saves the user to database', async () => {
@@ -90,24 +95,34 @@ describe('User Registration MongoDB', () => {
     expect(body.validationErrors).not.toBeUndefined();
   });
 
+  const username_null = 'Username cannot be null';
+  const username_size = 'Must have min 4 and max 32 characters';
+  const email_null = 'Email cannot be null';
+  const email_invalid = 'Email is not valid';
+  const password_null = 'Password cannot be null';
+  const password_size = 'Password must be at least 6 characters';
+  const password_pattern = 'Password must have at least 1 uppercase, 1 lowercase letter and 1 number';
+  const email_inuse = 'E-mail in use';
+  const user_create_success = 'User created';
+
   it.each`
     field         | value                     | expectedMessage
-    ${'username'} | ${null}                   | ${'Username cannot be null'}
-    ${'username'} | ${'usr'}                  | ${'Must have min 4 and max 32 characters'}
-    ${'username'} | ${'a'.repeat(33)}  | ${'Must have min 4 and max 32 characters'}
-    ${'email'}    | ${null}                   | ${'Email cannot be null'}
-    ${'email'}    | ${'mail.com'}             | ${'Email is not valid'}
-    ${'email'}    | ${'user.mail.com'}        | ${'Email is not valid'}
-    ${'email'}    | ${'user@mail'}            | ${'Email is not valid'}
-    ${'password'} | ${null}                   | ${'Password cannot be null'}
-    ${'password'} | ${'pass'}                 | ${'Password must be at least 6 characters'}
-    ${'password'} | ${'alllowercase'}         | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
-    ${'password'} | ${'ALLUPPERCASE'}         | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
-    ${'password'} | ${'1234567890'}           | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
-    ${'password'} | ${'lowerandUPPER'}        | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
-    ${'password'} | ${'lower4nd23423'}        | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
-    ${'password'} | ${'UPPER423423'}          | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
-  `('returns $expectedMessage when $field is $value', async ({field, expectedMessage, value}) => {
+    ${'username'} | ${null}                   | ${username_null}
+    ${'username'} | ${'usr'}                  | ${username_size}
+    ${'username'} | ${'a'.repeat(33)}  | ${username_size}
+    ${'email'}    | ${null}                   | ${email_null}
+    ${'email'}    | ${'mail.com'}             | ${email_invalid}
+    ${'email'}    | ${'user.mail.com'}        | ${email_invalid}
+    ${'email'}    | ${'user@mail'}            | ${email_invalid}
+    ${'password'} | ${null}                   | ${password_null}
+    ${'password'} | ${'pass'}                 | ${password_size}
+    ${'password'} | ${'alllowercase'}         | ${password_pattern}
+    ${'password'} | ${'ALLUPPERCASE'}         | ${password_pattern}
+    ${'password'} | ${'1234567890'}           | ${password_pattern}
+    ${'password'} | ${'lowerandUPPER'}        | ${password_pattern}
+    ${'password'} | ${'lower4nd23423'}        | ${password_pattern}
+    ${'password'} | ${'UPPER423423'}          | ${password_pattern}
+      `('returns $expectedMessage when $field is $value', async ({field, expectedMessage, value}) => {
     const user = {
       username: 'user1mongo',
       email: 'user1@gmail.com',
@@ -134,9 +149,73 @@ describe('User Registration MongoDB', () => {
   it('returns E-mail in use when same email is already in use', async () => {
     await UserModel.create({ ...validUser});
     const response = await postUser();
-    expect(response.body.validationErrors.email).toBe('E-mail in use');
+    expect(response.body.validationErrors.email).toBe(email_inuse);
   });
 
 });
 
+describe(`Internationalisation`, () => {
+  const MONGODB_TEST_DATABASE_URL = 'mongodb://localhost:27017/hoax-app_test';
 
+  beforeAll(async () => {
+    await connect(MONGODB_TEST_DATABASE_URL);
+  });
+
+  beforeEach(async () => {
+    await UserModel.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await disconnect();
+  });
+
+  const username_null = 'Username kann nicht null sein';
+  const username_size = 'Muss zwischen 4 und 32 Zeichen haben';
+  const email_null = 'Email kann nicht null sein';
+  const email_invalid = 'Email ist nicht korrekt';
+  const password_null = 'Password kann nicht null sein';
+  const password_size = 'Password muss mindestens 6 Zeichen lang sein';
+  const password_pattern = 'Password muss mindestens aus 1 Kleinbuchstaben, 1 Grossbuchstaben und 1 Zahl bestehen';
+  const email_inuse = 'E-mail wird bereits verwendet';
+  const user_create_success = 'User wurde erstellt';
+
+  it.each`
+    field         | value                     | expectedMessage
+    ${'username'} | ${null}                   | ${username_null}
+    ${'username'} | ${'usr'}                  | ${username_size}
+    ${'username'} | ${'a'.repeat(33)}  | ${username_size}
+    ${'email'}    | ${null}                   | ${email_null}
+    ${'email'}    | ${'mail.com'}             | ${email_invalid}
+    ${'email'}    | ${'user.mail.com'}        | ${email_invalid}
+    ${'email'}    | ${'user@mail'}            | ${email_invalid}
+    ${'password'} | ${null}                   | ${password_null}
+    ${'password'} | ${'pass'}                 | ${password_size}
+    ${'password'} | ${'alllowercase'}         | ${password_pattern}
+    ${'password'} | ${'ALLUPPERCASE'}         | ${password_pattern}
+    ${'password'} | ${'1234567890'}           | ${password_pattern}
+    ${'password'} | ${'lowerandUPPER'}        | ${password_pattern}
+    ${'password'} | ${'lower4nd23423'}        | ${password_pattern}
+    ${'password'} | ${'UPPER423423'}          | ${password_pattern}
+  `('returns $expectedMessage when $field is $value when language is set as german', async ({field, expectedMessage, value}) => {
+    const user = {
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: 'password'
+    };
+    user[field] = value;
+    const response = await postUser(user, {language: 'de'});
+    const body = response.body;
+    expect(body.validationErrors[field]).toBe(expectedMessage);
+  });
+
+  it(`returns ${email_inuse} when same email is already in use when language is set as german`, async () => {
+    await UserModel.create({ ...validUser});
+    const response = await postUser({ ...validUser}, {language: 'de'});
+    expect(response.body.validationErrors.email).toBe(email_inuse);
+  });
+
+  it(`returns success message of ${user_create_success} when signup request is valid and language is set as german`, async () => {
+    const response = await postUser({ ...validUser}, {language: 'de'});
+    expect(response.body.message).toBe(user_create_success);
+  });
+});
